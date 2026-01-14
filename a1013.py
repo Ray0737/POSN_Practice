3e47116a47c9a9c825c321a691c0f8b2a12c8dd5
c0der <raphee.rattanamanoonporn@gmail.com>
2026-01-15 05:49:25 +0700

    Update a1013.py

--- a1013.py
+++ a1013.py
@@ -3,11 +3,8 @@ num = int(input("Enter 4 digit code: "))
 
 correct_char = "H"
 correct_num = 4567
-
-if char == correct_char and num == correct_num:
-    print("safe unlocked")
-
-elif char == correct_char and num != correct_num:
+
+if char == correct_char and num != correct_num:
     print("safe locked - change digit")
 
 elif char != correct_char and num == correct_num:
